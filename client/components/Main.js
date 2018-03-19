6a95431c332f8e10da174e4a349cb08ef22631d6
Caio Godoi <caio.godoi@gmail.com>
2018-03-19 15:24:09 -0300

    create router

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import PhotoGrid from './PhotoGrid';
 
 const Main = () => {
   return (
@@ -7,7 +8,7 @@ const Main = () => {
       <h1>
         <Link to="/">Reduxstagram</Link>
       </h1>
-      {React.cloneElement(this.props.children, this.props)}
+      <PhotoGrid />
     </div>
   );
 };
