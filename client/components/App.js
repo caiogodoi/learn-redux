601055b07164a796f22a34ba262c30a4bd719e2a
Caio Godoi <caio.godoi@gmail.com>
2018-03-22 15:41:32 -0300

    create app

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments,
+  };
+}
+
+function MapDispatchToProps(dispatch) {
+  return bindActionCreators(actionCreators, dispatch);
+}
+
+const app = connect(mapStateToProps, MapDispatchToProps)(Main);
+
+export default App;
